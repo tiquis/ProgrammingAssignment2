237d0c1b202503bda3277d996ef124dfd7c49daa
Rafael Magallanes Quintanar <tiquis@gmail.com>
2020-06-07 16:25:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Store input matrix and creates cache of inverse matrix "cm"
+##
+## set function set the inverse to NULL
+## get function retreives the cached matrix
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cm <- NULL
+  set <- function(y){
+      cm <<- NULL
+  }
+  get <- function() x
+    setinv <- function(inverse) cm <<- inverse
+    getinv <- function() cm
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
-
-
 ## Write a short comment describing this function
-
+##
+## Return a matrix that is the inverse of 'x', retrieves the cached matrix if exists
+##
+## getinv gets the inverse of matrix
+## setinv calculates the inverse of matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cm <- x$getinv()
+  if(!is.null(cm)) {
+    message("getting cached data")
+    return(cm)
+  }
+  a <- x$get()
+  cm <- solve(a)
+  x$setinv(cm)
+  cm
 }
+## EXAMPLE OF USE
+## 
+##  > a=rbind(c(1, -1/4), c(-1/4, 1))
+##  > a
+##  [,1]  [,2]
+##  [1,]  1.00 -0.25
+##  [2,] -0.25  1.00
+##  > b <- makeCacheMatrix(a)
+##  > c <- cacheSolve(b)
+##  > d <- cacheSolve(b)
+##  getting cached data if matrix exist
